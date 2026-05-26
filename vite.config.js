381bcb0addc34d1dfb183f458d5235c020ecbf37
daear <daearth081102@gmail.com>
2026-05-26 18:27:15 +0900

    configure moodpit2 deploy path

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,6 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
 export default defineConfig({
-  base: process.env.VERCEL ? "/" : "/moodpit/",
+  base: process.env.VERCEL ? "/" : "/moodpit2/",
   plugins: [react()],
 });
